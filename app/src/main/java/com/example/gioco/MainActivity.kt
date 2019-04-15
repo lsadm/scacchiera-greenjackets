c5b4a4c38343a103cc53919b5de3157dbc595cfa
Heisenberg <simone.cristofaro@studenti.unicampania.it>
2019-04-15 21:10:56 +0200

    Lettura coordinate: -Legge il tap dopo che è stato alzato il dito da schermo -Notifica Toast delle coordinate -Implementazione di array con valori 1(nero) e 0(bianco) per poter implementare l'inversione della casella. Non ancora funzionante.

--- app/src/main/java/com/example/gioco/MainActivity.kt
+++ app/src/main/java/com/example/gioco/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.gioco
 import android.graphics.Canvas
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.MotionEvent
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
@@ -28,7 +29,7 @@ class MainActivity : AppCompatActivity() {
             override fun onProgressChanged(seekBar: SeekBar?, progress: Int, fromUser: Boolean) { //cose da fare quando cambia il valore
                 div=progress+2              //setto il minimo della seekbar a 2, visto che parte da 0
                 div_count.text= div.toString()
-                vScacchiera.divisions=div
+                vScacchiera.div=div
 
             }
             override fun onStartTrackingTouch(seekBar: SeekBar?) {}         //devo per forza fare l'ovveride, anche se non le specifico
@@ -42,6 +43,20 @@ class MainActivity : AppCompatActivity() {
             vScacchiera.invalidate()        //Serve per invalidare la view e forzare la chiamata del metodo onDraw di ScacchieraView
             mosse.text= 0.toString()
         }
+
+
+        vScacchiera.setOnTouchListener(View.OnTouchListener { _, motionEvent ->
+            when (motionEvent.action){
+
+                MotionEvent.ACTION_UP -> {
+                    var x = motionEvent.x.toInt()
+                    var y = motionEvent.y.toInt()
+                    Toast.makeText(this@MainActivity, "X: "+x+" Y: "+y, Toast.LENGTH_SHORT).show()
+
+                }
+            }
+            return@OnTouchListener true
+        })
 
 
     }
--- app/src/main/java/com/example/gioco/ScacchieraView.kt
+++ app/src/main/java/com/example/gioco/ScacchieraView.kt
@@ -6,8 +6,10 @@ import android.graphics.Color
 import android.graphics.Paint
 import android.graphics.Rect
 import android.util.AttributeSet
+import android.view.MotionEvent
 import android.view.View
 import android.widget.TextView
+import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_main.view.*
 import org.w3c.dom.Text
 
@@ -21,21 +23,28 @@ class ScacchieraView: View {
     var schermo = Rect()
     val Nero = Paint()
     val Bianco = Paint()
-    var divisions =2        //ho lasciato divisions fuori per renderla accessibile dalla main activity
 
+    var div:Int =2        //ho lasciato divisions fuori per renderla accessibile dalla main activity
+    var dxt=0
+    var dyt=0
+
+
+   // var valori = arrayOfNulls<Int>(div*div)
 
     override fun onDraw(canvas: Canvas?) {  //il canvas è la nostra tela quindi oltre non possiamo andare,attenzione
         super.onDraw(canvas)
 
-        var div = divisions
-
         canvas?.getClipBounds(schermo)        // mi prende la dimensione dello schermo con ClipBounds
+
+        var dx = (schermo.right-schermo.left)/div
+        var dy = (schermo.bottom-schermo.top)/div
+        dxt=dx
+        dyt=dy
 
         Nero.color= Color.BLACK     //Posso settare il colore del pennello solo in onDraw
         Bianco.color = Color.WHITE
 
-        var dx = (schermo.right-schermo.left)/div
-        var dy = (schermo.bottom-schermo.top)/div
+
 
         for (i in 0..div){
             for (j in 0..div){
@@ -46,32 +55,37 @@ class ScacchieraView: View {
 
                 var casella = Rect(left,top,right,bottom)       //crea la casella nella posizione corretta
 
-                if((i+j)%2==0)
+                if((i+j)%2==0){
                     canvas?.drawRect(casella, Nero)        //dice dipingi sulla casella con il pennello.Mettere il ? per sicurezza.
-                else
+                   // valori.set(j + (i * div),1)
+                    }
+
+                else{
                     canvas?.drawRect(casella, Bianco)
+                    //valori.set(j + (i * div),0)
+                }
+
+
+
             }
 
+        }//fine for innestato
+/*
+        for (element in valori) {
+            println(element)
         }
-
-
+*/
 
     }
-
-
-
-
 
 /*
-    override fun onTouchEvent(event: MotionEvent?): Boolean {
-
-        return super.onTouchEvent(event)   //in event c'è il significato del tocco
-        if(event?.action==MotionEvent.ACTION_UP) {     //MotionEvent ha molti metodi ! L'evento ha l'info action, a noi interessa l'action UP
-            var x = event?.x          // Action Up legge quando il dito se ne va
-            var y = event?.y
-        }
-    }
+   fun AggiornaCasella(x:Int , y:Int) {
+       var i=x/dx
+       var j=y/dy
+      // canvas?.drawRect(casella, Bianco)
+   }
 */
+
 
 
 
